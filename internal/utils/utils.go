297b89b3070327150b1dd3705873fd7cca1b0af1
Augusto Antonio <augusto.schiavini@gmail.com>
2025-03-06 19:56:59 -0300

    testes com linhas e colunas feito

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,29 +1,40 @@
 package utils
 
-import {
-    "fmt"
+import (
     "os"
     "bufio"
+    "errors"
+    "strings"
+    "fmt"
+)
+
+
+type FileInfo struct {
+
+    NumLines int
+    NumCols int
+    WordSpace int
+
 }
 
 
-type fileInfo struct {
-
-    numLines int
-    numCol int
-    wordSpace int
-
-}
-
-
-
+//------------------------------------------------------------------
 func GetLines( file *os.File) (int, error) {
 
     numLines := 0
+    var err error
 
-    if file == nil
+    if file == nil {
         return 0, errors.New("GetLines: no file")
+    }
         
+    //Reset pointer to the beginning of the file
+    _, err = file.Seek(0, 0)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
 
@@ -34,8 +45,8 @@ func GetLines( file *os.File) (int, error) {
 
 }
 
-func SplitByCommas( data []byte, atEOF bool) (advanced int, 
-token []byte, err error) {
+//------------------------------------------------------------------
+func SplitByCommas( data []byte, atEOF bool) (advanced int,token []byte, err error) {
 
     if atEOF && len(data) == 0 {
         return 0, nil, nil
@@ -48,54 +59,110 @@ token []byte, err error) {
         }
     }
 
-
-    }
     if atEOF {
         return len(data), data, nil
     }
     return
 
 }
+//------------------------------------------------------------------
 
-func GetCols (file *os.File) int {}
+func GetCols (file *os.File) (int, error) {
 
-func GetMatrix (file *os.File, numLines int, numCols int) [][]string {}
-
-func getInfo (file *os.File) (fileInfo, error) {
-
-    info *fileInfo
+    numCols := 0
+    var err error
 
     if file == nil {
-        return 0, errors.New("GetInfo: no file")
+        return 0, errors.New("GetCols: no file")
     }
-     
-    numLines := 0   
-    numCols := 0
 
-    //pegar o numero de linhas
-    //adicionar um reader para pegar a primeira linha com o numero de colunas
-    // pegar palavra por palavra e por num slice 2d
-
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-
-
+    //Reset pointer to the beginning of the file
+    _, err = file.Seek(0, 0)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
 
     scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-
-        numLines++
-        wordScanner := bufio.NewScanner(scanner.Text)
-        scanner.Split(SplitByCommas)
-
+    if scanner.Scan() {
         
+        for range strings.Split(scanner.Text(),",") {
+
+            numCols++
+
+        }
 
     }
-
-
+    return numCols, nil
 
 
 }
+
+
+//------------------------------------------------------------------
+func GetWordSpace (file *os.File) (int, error) {
+
+    wordSpace := 0
+    var err error
+
+    if file == nil {
+        return 0, errors.New("GetWordSpace: no file")
+    }
+
+    //Reset pointer to the beginning of the file
+    _, err = file.Seek(0, 0)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+
+    scanner := bufio.NewScanner(file)
+    scanner.Split(SplitByCommas)
+
+    for scanner.Scan(){
+    
+        fmt.Println(scanner.Text())
+        if len(scanner.Text()) > wordSpace {
+            fmt.Println(wordSpace)
+            wordSpace = len(scanner.Text())
+        }
+    }
+    return wordSpace, nil
+
+}
+
+//------------------------------------------------------------------
+//func GetMatrix (file *os.File, numLines int, numCols int) [][]string {}
+
+//------------------------------------------------------------------
+func GetInfo (file *os.File) (*FileInfo, error) {
+
+    //info *FileInfo
+    info := new(FileInfo)
+    var err error
+
+    if file == nil {
+        return nil, errors.New("GetInfo: no file")
+    }
+     
+    info.NumLines, err = GetLines(file)   
+    if (err != nil){
+        return nil, err
+    }
+
+    info.NumCols, err = GetCols(file)  
+    if (err != nil){
+        return nil, err
+    }
+
+    info.WordSpace, err = GetWordSpace(file)
+    if (err != nil){
+        return nil, err
+    }
+    return info, nil
+
+ }
+//------------------------------------------------------------------
 
 
 
